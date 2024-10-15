223ca2a47a73ce4e2826a153b1556ca7ec58aa66
ldmaximino <ldmaximino@gmail.com>
2024-10-15 15:05:45 -0300

    update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://apartkeka.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://apartkeka.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
